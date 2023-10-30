205136d570956bebd4d11e9b31b926e1129fa1d2
jessik126 <jessik126@gmail.com>
2023-10-30 15:44:45 -0300

    create different routes: simple, with param, 404

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -2,14 +2,18 @@ import './Content.css'
 import React from "react";
 import {Routes, Route} from 'react-router-dom'
 
-import Home from '../../views/examples/Home';
-import About from '../../views/examples/About';
+import Home from '../../views/pages/Home';
+import About from '../../views/pages/About';
+import Param from '../../views/pages/Param';
+import NotFound from '../../views/pages/NotFound';
 
 const Content = props => (
     <main className="Content">
         <Routes>
             <Route path="/about" element={<About/>}/>
+            <Route path="/param/:id" element={<Param/>}/>
             <Route path="/" element={<Home/>}/>
+            <Route path="*" element={<NotFound/>}/>
         </Routes>
     </main>
 )
--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import './Menu.css'
 
+import { Link } from 'react-router-dom'
+
 const Menu = props => (
     <aside className="Menu">
         <nav>
             <ul>
                 <li>
-                    <a href="/">Home</a>
+                    <Link to="/">Inicio</Link>
                 </li>
                 <li>
-                    <a href="/">About</a>
+                    <Link to="/param/123">Param #01</Link>
+                </li>
+                <li>
+                    <Link to="/param/text">Param #02</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                    {/* <a href="/about">About</a> */}
+                </li>
+                <li>
+                    <Link to="/nothing">Nothing</Link>
                 </li>
             </ul>
         </nav>
